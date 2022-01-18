3409928dee8ddf5f33f7cac2e8197b8990471935
Nikola Manolov <nikola.manolov20@gmail.com>
2022-01-19 01:47:13 +0200

    Create dictionaryController.h

---
+++ dictionaryController.h
@@ -0,0 +1,41 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Nikola Manolov
+* @idnumber 5MI0600093
+* @compiler GCC
+*
+* <files for actions with the dictionary>
+*
+*/
+
+#ifndef SCRABBLE_DICTIONARYCONTROLLER_H
+#define SCRABBLE_DICTIONARYCONTROLLER_H
+
+#endif //SCRABBLE_DICTIONARYCONTROLLER_H
+
+#include <fstream>
+
+const char path[] = "../words.txt";
+
+bool isRealWord(string word) {
+    fstream dictionary;
+
+    dictionary.open(path, fstream::in);
+    if (!dictionary.is_open()) {
+        std::cout << "System error! Could not open dictionary!" << endl;
+        return false;
+    }
+
+    string vocabulary;
+    while (getline(dictionary, vocabulary)) {
+        if (word == vocabulary) return true;
+    }
+
+    return false;
+
+}
