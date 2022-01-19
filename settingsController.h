ebec35af3d1801771067ea8329da8039194fd005
Nikola Manolov <nikola.manolov20@gmail.com>
2022-01-19 12:06:02 +0200

    Create settingsController.h

---
+++ settingsController.h
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Nikola Manolov
+* @idnumber 5MI0600093
+* @compiler GCC
+*
+* <file for settings functions>
+*
+*/
+
+#ifndef SCRABBLE_SETTINGSCONTROLLER_H
+#define SCRABBLE_SETTINGSCONTROLLER_H
+#endif
+
+
+
